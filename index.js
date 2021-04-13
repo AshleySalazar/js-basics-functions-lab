fc758134fee361642ba474dbf9db168e14ef5122
Ashley Salazar <ashleysalazar0902@gmail.com>
2021-04-13 15:15:10 -0500

    commit tree 1830688b9c5afd2a43d3b1af5abdd2fcc5a00281

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation){
+    const blocks = Math.abs(pickupLocation - 42);
+    return blocks; 
+};
+
+function distanceFromHqInFeet(pickupLocation){
+    const feet = Math.abs( (pickupLocation - 42) * 264);
+    return feet;
+};
+
+function distanceTravelledInFeet(pickupLocation, dropoffLocation){
+    const feet = Math.abs( (pickupLocation - dropoffLocation) * 264);
+    return feet; 
+};
+
+function calculatesFarePrice(start, destination){
+    let result;
+    const feet = Math.abs( (start - destination) * 264);
+
+    if (feet < 400){ 
+        result = 0
+    } else if (feet >= 400 && feet <= 2000) {
+       result = (feet - 400) * 0.02;
+    } else if ( feet > 2000 && feet <= 2500){
+        result = 25;
+    } else {
+        result = ("cannot travel that far")
+    }
+
+    return result;
+}
